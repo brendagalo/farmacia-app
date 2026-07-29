16e6ecacb0a54b16049c450cec05da6d41508655
jbriones <jbriones600@gmail.com>
2026-07-29 12:52:26 -0600

    icono 29-07-26 Modulo Buckup, modulo proveedor y modulo categoria

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farmacia System</title>
+    <title>Farmacia Galo</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.ico') }}">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
